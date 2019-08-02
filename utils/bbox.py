1afd6e25b5c84614fdee08f00a8af90deebe653b
Diego Castro <diegocastroviadero19@gmail.com>
2019-08-02 19:40:20 +0200

    added text to predicting bounding boxes showing their probability

--- utils/bbox.py
+++ utils/bbox.py
@@ -64,26 +64,27 @@ def draw_boxes(image, boxes, labels, obj_thresh, quiet=True):
         for i in range(len(labels)):
             if box.classes[i] > obj_thresh:
                 if label_str != '': label_str += ', '
-                label_str += (labels[i] + ' ' + str(round(box.get_score()*100, 2)) + '%')
+                #label_str += (labels[i] + ' ' + str(round(box.get_score()*100, 2)) + '%')
+                label_str += (str(int(round(box.get_score()*100))) + '%')
                 label = i
             if not quiet: print(label_str)
                 
         if label >= 0:
             #text_size = cv2.getTextSize(label_str, cv2.FONT_HERSHEY_SIMPLEX, 1.1e-3 * image.shape[0], 5)
             #width, height = text_size[0][0], text_size[0][1]
-            #region = np.array([[box.xmin-3,        box.ymin], 
-            #                   [box.xmin-3,        box.ymin-height-26], 
-            #                   [box.xmin+width+13, box.ymin-height-26], 
-            #                   [box.xmin+width+13, box.ymin]], dtype='int32')  
+            #region = np.array([[box.xmin-3,        box.ymin],
+            #                   [box.xmin-3,        box.ymin-height-26],
+            #                   [box.xmin+width+13, box.ymin-height-26],
+            #                   [box.xmin+width+13, box.ymin]], dtype='int32') 
 
             cv2.rectangle(img=image, pt1=(box.xmin,box.ymin), pt2=(box.xmax,box.ymax), color=get_color(label), thickness=1)
             #cv2.fillPoly(img=image, pts=[region], color=get_color(label))
-            #cv2.putText(img=image, 
-            #            text=label_str, 
-            #            org=(box.xmin+13, box.ymin - 13), 
-            #            fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
-            #            fontScale=1e-3 * image.shape[0], 
-            #            color=(0,0,0), 
-            #            thickness=2)
+            cv2.putText(img=image, 
+                        text=label_str, 
+                        org=(box.xmin, box.ymin - 2),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1e-3 * image.shape[0] * 0.8,
+                        color=(0, 0, 255),
+                        thickness=1)
         
     return image          
