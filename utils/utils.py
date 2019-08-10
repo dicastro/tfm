31abc030a356bfb0416d54942d81b9d78b82edfb
Diego Castro <diegocastroviadero19@gmail.com>
2019-08-10 18:16:54 +0200

    Fix bug in generalization of get_yolo_boxes function to be compatible with yolo v3 and tiny versions

--- utils/utils.py
+++ utils/utils.py
@@ -252,8 +252,11 @@ def get_yolo_boxes(model, images, net_h, net_w, anchors, obj_thresh, nms_thresh)
         boxes = []
 
         # decode the output of the network
+        size = (len(anchors)//2)//len(yolos)
+        e = len(yolos)
+        s = e - 1
         for j in range(len(yolos)):
-            yolo_anchors = anchors[(2-j)*6:(3-j)*6] # config['model']['anchors']
+            yolo_anchors = anchors[(s-j)*size:(e-j)*size] # config['model']['anchors']
             boxes += decode_netout(yolos[j], yolo_anchors, obj_thresh, net_h, net_w)
 
         # correct the sizes of the bounding boxes
