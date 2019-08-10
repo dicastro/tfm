05ef4eb0ece555f130f50455d7fb53538ae5d850
Diego Castro <diegocastroviadero19@gmail.com>
2019-08-10 11:04:56 +0200

    Catched exception for debugging purposes

--- yolo3/utils.py
+++ yolo3/utils.py
@@ -106,7 +106,11 @@ def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jit
     box_data = np.zeros((max_boxes,5))
     if len(box)>0:
         np.random.shuffle(box)
-        box[:, [0,2]] = box[:, [0,2]]*nw/iw + dx
+        try:
+            box[:, [0,2]] = box[:, [0,2]]*nw/iw + dx
+        except Exception e:
+            print('Box: {}, nw: {}, iw: {}, dx: {} - line: {}'.format(box, nw, iw, dx, annotation_line))
+            raise e
         box[:, [1,3]] = box[:, [1,3]]*nh/ih + dy
         if flip: box[:, [0,2]] = w - box[:, [2,0]]
         box[:, 0:2][box[:, 0:2]<0] = 0
